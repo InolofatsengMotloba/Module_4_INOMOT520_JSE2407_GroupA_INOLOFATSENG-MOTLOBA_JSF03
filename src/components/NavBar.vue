bf64ae5c2f2816e64ebc71a3127023d70d594737
Inolofatseng Motloba <motlobainolo123@gmail.com>
2024-08-04 20:50:35 +0200

    Added Navigation toggle button for mobile view and conditional visibility to navbar dropdown

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -18,8 +18,39 @@
             </span>
           </button>
         </a>
+        <button
+          @click="toggleNavbar"
+          type="button"
+          class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-white rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
+          aria-controls="navbar-dropdown"
+        >
+          <span class="sr-only">Open main menu</span>
+          <svg
+            class="w-5 h-5"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 17 14"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M1 1h15M1 7h15M1 13h15"
+            />
+          </svg>
+        </button>
 
-        <div class="w-full md:block md:w-auto">
+        <div
+          :class="{
+            hidden: !isNavbarVisible,
+            'w-full': true,
+            'md:block': true,
+            'md:w-auto': true,
+          }"
+          id="navbar-dropdown"
+        >
           <ul
             class="flex flex-col top-10 font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-500 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0"
           >
@@ -80,8 +111,20 @@
 </template>
 
 <script>
+import { ref } from "vue";
+
 export default {
   name: "NavBar",
+
+  data() {
+    const isNavbarVisible = ref(false);
+
+    const toggleNavbar = () => {
+      isNavbarVisible.value = !isNavbarVisible.value;
+    };
+
+    return { isNavbarVisible, toggleNavbar };
+  },
 };
 </script>
 
