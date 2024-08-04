eaf327472c83132cd70d411bdd9e10e6d6f6dd0d
Inolofatseng Motloba <motlobainolo123@gmail.com>
2024-08-04 19:59:40 +0200

    Updated NavBar component with new HTML structure, Logo image and styles

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,12 +1,31 @@
 <template>
-  <header>
-    <h1>NavBar goes Here</h1>
+  <header class="sticky z-50 top-0">
+    <nav class="bg-gray-500 border-gray-200">
+      <div
+        class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4"
+      >
+        <a href="/">
+          <button class="flex items-center space-x-3 rtl:space-x-reverse">
+            <img
+              src="/public/images/online-shop.png"
+              class="h-8"
+              alt="Flowbite Logo"
+            />
+            <span
+              class="self-center text-2xl font-semibold whitespace-nowrap text-white"
+            >
+              SwiftCart
+            </span>
+          </button>
+        </a>
+      </div>
+    </nav>
   </header>
 </template>
 
 <script>
 export default {
-  name: 'NavBar',
+  name: "NavBar",
 };
 </script>
 
