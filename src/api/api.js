05ccfda5795acec95f8c50fbb6ec9dc6940bcffd
Inolofatseng Motloba <motlobainolo123@gmail.com>
2024-08-05 09:58:25 +0200

    Added fetchSingleProduct function to api.js

--- src/api/api.js
+++ src/api/api.js
@@ -14,3 +14,18 @@ export const getCategories = async () => {
     return { error: error };
   }
 };
+
+export const fetchSingleProduct = async (id) => {
+  try {
+    const response = await fetch(`https://fakestoreapi.com/products/${id}`);
+    if (!response.ok) {
+      throw new Error(
+        "Data fetching failed :( , please check your network connection and reload."
+      );
+    }
+    const data = await response.json();
+    return { response: data };
+  } catch (error) {
+    return { error: error };
+  }
+};
