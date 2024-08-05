d2f9a1746a544e45aaef30d694773aeb247a1692
Inolofatseng Motloba <motlobainolo123@gmail.com>
2024-08-05 09:45:46 +0200

    Updated Home.vue to add Filter, Sort, and ProductsList components, and modified template structure

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,11 +1,20 @@
 <template>
-  <div>
-    <h1>Home page</h1>
+  <div
+    class="grid lg:flex gap-y-4 gap-x-48 lg:items-start mt-3 mx-auto justify-center"
+  >
+    <Filter />
+    <Sort />
   </div>
+  <products-list />
 </template>
 
 <script>
+import Filter from '../components/Filter.vue';
+import ProductsList from '../components/products/ProductsList.vue';
+import Sort from '../components/Sort.vue';
+
 export default {
-  name: 'Home',
+  components: { ProductsList, Filter, Sort, Filter },
+  name: "Home",
 };
 </script>
