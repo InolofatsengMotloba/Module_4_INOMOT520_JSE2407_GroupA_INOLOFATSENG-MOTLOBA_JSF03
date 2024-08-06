6d0d069180bc2d4b4a04c0bbd328822750f424a6
Inolofatseng Motloba <motlobainolo123@gmail.com>
2024-08-06 13:14:40 +0200

    Updated ProductsList.vue to use filteredProducts instead of products as updated in ProductStore

--- src/components/products/ProductsList.vue
+++ src/components/products/ProductsList.vue
@@ -2,7 +2,7 @@
   <div class="grid justify-center">
     <div class="grid gap-5 grid-cols-1 md:grid-cols-2 lg:grid-cols-4 my-4">
       <div
-        v-for="product in products"
+        v-for="product in filteredProducts"
         :key="product.id"
         class="flex flex-col max-h-[130rem] cursor-pointer max-w-80 hover:-translate-y-1 hover:scale-105 duration-300 bg-white border border-slate-200 shadow shadow-slate-950/5 rounded-2xl overflow-hidden"
       >
@@ -121,7 +121,7 @@ export default {
     });
 
     return {
-      products: computed(() => productStore.products),
+      filteredProducts: computed(() => productStore.filteredProducts),
     };
   },
 };
