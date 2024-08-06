d1b05db1390b2b5a46a86874f8ceac6920369dda
Inolofatseng Motloba <motlobainolo123@gmail.com>
2024-08-06 13:22:35 +0200

    Updated Filter.vue component: Added styling and reformatted HTML structure

--- src/components/Filter.vue
+++ src/components/Filter.vue
@@ -1,7 +1,10 @@
 <template>
-  <div>
-    <h1>Filter:</h1>
-    <select @change="onCategoryChange($event)">
+  <div class="flex sm:w-[95%] max-w-[21rem] md:w-full">
+    <label for="category" class="w-20 my-auto font-semibold"> Filter: </label>
+    <select
+      @change="onCategoryChange($event)"
+      class="p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-e-lg border-s-gray-50 border-s-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
+    >
       <option value="all">All Categories</option>
       <option v-for="category in categories" :key="category" :value="category">
         {{ category }}
