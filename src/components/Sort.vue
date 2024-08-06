43d20fde0adfa81d4a4929b99a4a3a8ca3d827ac
Inolofatseng Motloba <motlobainolo123@gmail.com>
2024-08-06 13:26:47 +0200

    Updated Sort.vue component: changed HTML structure, Added styling and modified select element

--- src/components/Sort.vue
+++ src/components/Sort.vue
@@ -1,7 +1,7 @@
 <template>
-  <div>
-    <h1>Sort by:</h1>
-    <select @change="onSortChange($event)">
+  <div class="flex sm:w-[95%] max-w-[21rem] md:w-full">
+    <label for="category" class="w-20 my-auto font-semibold"> Sort by: </label>
+    <select @change="onSortChange($event)" class="p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-e-lg border-s-gray-50 border-s-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500">
       <option value="default">Default</option>
       <option value="asc">Price: Low to High</option>
       <option value="desc">Price: High to Low</option>
