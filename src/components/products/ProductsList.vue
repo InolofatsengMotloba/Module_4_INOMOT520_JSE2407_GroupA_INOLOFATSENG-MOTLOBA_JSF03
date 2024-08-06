6a453e2ce9e4fad1fae1ca70c595a9221582b350
Inolofatseng Motloba <motlobainolo123@gmail.com>
2024-08-06 03:41:33 +0200

    Updated ProductsList.vue to display product titles in a list & added setup function to fetch products on mount

--- src/components/products/ProductsList.vue
+++ src/components/products/ProductsList.vue
@@ -1,11 +1,30 @@
 <template>
   <div>
-    <h1>Product List</h1>
+    <ul>
+      <li v-for="product in products" :key="product.id">{{ product.title }}</li>
+    </ul>
   </div>
 </template>
 
 <script>
+import { onMounted } from 'vue';
+import { useProductStore } from '../../store/productStore';
+
 export default {
   name: 'ProductList',
+  setup() {
+    const productStore = useProductStore();
+    
+    onMounted(() => {
+      productStore.fetchProducts();
+    });
+
+    return {
+      products: productStore.products,
+    };
+  },
 };
 </script>
+
+<style scoped>
+</style>
