6198a2160ecd4bacc0e42d45ec8581053861e60e
Inolofatseng Motloba <motlobainolo123@gmail.com>
2024-08-06 04:39:44 +0200

    Added error handling and product data fetching by id to ProductDetailView.vue

--- src/pages/ProductDetailView.vue
+++ src/pages/ProductDetailView.vue
@@ -1,11 +1,50 @@
 <template>
   <div>
-    <h1>Product Detail View page</h1>
+    <div v-if="error">There was an error</div>
+    <div v-else-if="product">
+      <h2>{{ product.title }}</h2>
+    </div>
+    <div v-else>Loading...</div>
   </div>
 </template>
 
 <script>
+import { ref, onMounted } from "vue";
+import { fetchSingleProduct } from "../api/api";
+
 export default {
-  name: 'ProductDetailView',
+  name: "ProductDetailView",
+  props: {
+    id: {
+      type: [String, Number],
+      required: true,
+    },
+  },
+  setup(props) {
+    const product = ref(null);
+    const error = ref(null);
+
+    const getProduct = async () => {
+      const { response, error: fetchError } = await fetchSingleProduct(
+        props.id
+      );
+      if (fetchError) {
+        error.value = fetchError.message;
+      } else {
+        product.value = response;
+      }
+    };
+
+    onMounted(() => {
+      getProduct();
+    });
+
+    return {
+      product,
+    };
+  },
 };
 </script>
+
+<style scoped>
+</style>
