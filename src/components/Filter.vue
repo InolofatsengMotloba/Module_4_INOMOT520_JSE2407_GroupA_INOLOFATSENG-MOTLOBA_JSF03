69d2b7dd3ba9e41f04c41efa5c8f9fd85b3317f9
Inolofatseng Motloba <motlobainolo123@gmail.com>
2024-08-06 13:11:29 +0200

    Added function to get categories from the API and dynamically populate options & Implemented 'onCategoryChange' function to emit the selected category

--- src/components/Filter.vue
+++ src/components/Filter.vue
@@ -1,11 +1,46 @@
 <template>
   <div>
-    <h1>Filter goes here</h1>
+    <h1>Filter:</h1>
+    <select @change="onCategoryChange($event)">
+      <option value="all">All Categories</option>
+      <option v-for="category in categories" :key="category" :value="category">
+        {{ category }}
+      </option>
+    </select>
   </div>
 </template>
 
 <script>
+import { onMounted, ref } from "vue";
+import { getCategories } from "../api/api";
+
 export default {
-  name: 'Filter',
+  name: "Filter",
+  emits: ["filter-category"],
+  setup(_, { emit }) {
+    const categories = ref([]);
+
+    const fetchCategories = async () => {
+      const { response, error } = await getCategories();
+      if (response) {
+        categories.value = response;
+      } else {
+        console.error(error);
+      }
+    };
+
+    const onCategoryChange = (event) => {
+      emit("filter-category", event.target.value);
+    };
+
+    onMounted(() => {
+      fetchCategories();
+    });
+
+    return {
+      categories,
+      onCategoryChange,
+    };
+  },
 };
 </script>
