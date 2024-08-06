26fb765a81de0704ac6d830a3a2f6f83775adfbc
Inolofatseng Motloba <motlobainolo123@gmail.com>
2024-08-06 13:14:02 +0200

    Added event handlers to Home.vue for filter-category and sort-order events

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -2,8 +2,8 @@
   <div
     class="grid lg:flex gap-y-4 gap-x-48 lg:items-start mt-3 mx-auto justify-center"
   >
-    <Filter />
-    <Sort />
+    <Filter @filter-category="filterByCategory" />
+    <Sort @sort-order="sortProducts" />
   </div>
   <ProductsList />
 </template>
@@ -12,9 +12,29 @@
 import Filter from "../components/Filter.vue";
 import ProductsList from "../components/products/ProductsList.vue";
 import Sort from "../components/Sort.vue";
+import { useProductStore } from "../store/productStore";
 
 export default {
   name: "Home",
   components: { ProductsList, Filter, Sort },
+  setup() {
+    const productStore = useProductStore();
+    // const route = useRoute();
+
+    const filterByCategory = (category) => {
+      productStore.filterByCategory(category);
+    };
+
+    const sortProducts = (order) => {
+      productStore.sortProducts(order);
+    };
+
+    return {
+      filterByCategory,
+      sortProducts,
+      productStore,
+
+    };
+  },
 };
 </script>
