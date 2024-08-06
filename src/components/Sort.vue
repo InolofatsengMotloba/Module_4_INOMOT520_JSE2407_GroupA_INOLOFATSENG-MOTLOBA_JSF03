7c7e4d70d83819b703a4294f6849094542d1812c
Inolofatseng Motloba <motlobainolo123@gmail.com>
2024-08-06 13:12:52 +0200

    Implemented 'onSortChange' method to emit selected sort order to parent component & Added sort options

--- src/components/Sort.vue
+++ src/components/Sort.vue
@@ -1,11 +1,26 @@
 <template>
   <div>
-    <h1>Sort goes here</h1>
+    <h1>Sort by:</h1>
+    <select @change="onSortChange($event)">
+      <option value="default">Default</option>
+      <option value="asc">Price: Low to High</option>
+      <option value="desc">Price: High to Low</option>
+    </select>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Sort',
+  name: "Sort",
+  emits: ["sort-order"],
+  setup(_, { emit }) {
+    const onSortChange = (event) => {
+      emit("sort-order", event.target.value);
+    };
+
+    return {
+      onSortChange,
+    };
+  },
 };
 </script>
